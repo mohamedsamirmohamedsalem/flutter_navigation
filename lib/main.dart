c1156ab1b7c85e29893a154e0198cfd55259aa11
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2020-01-15 11:36:56 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
+import 'package:flutter_dialogs/pages/auth.dart';
+
+void main() {
+  // debugPaintSizeEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  // debugPaintPointersEnabled = true;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // debugShowMaterialGrid: true,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
+      home: AuthPage(),
+    );
+  }
+}
